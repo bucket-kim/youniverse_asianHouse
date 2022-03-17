c09db42b51271f0f9831ae4e3e043ac414a4a3c8
Brian Kim <brian.kyounghoon.kim@gmail.com>
2022-03-17 12:40:26 -0400

    class modification

---
+++ src/Experience/Camera.js
@@ -0,0 +1,38 @@
+import * as THREE from "three";
+import Experience from "./Experience";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+export default class Camera {
+  constructor() {
+    this.experience = new Experience();
+    this.sizes = this.experience.sizes;
+    this.scene = this.experience.scene;
+    this.canvas = this.experience.canvas;
+
+    this.setCamera();
+    this.setControls();
+  }
+
+  setCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      this.sizes.width / this.sizes.height,
+      0.1,
+      1000
+    );
+    this.camera.position.set(6, 4, 8);
+    this.scene.add(this.camera);
+  }
+
+  setControls() {
+    this.controls = new OrbitControls(
+      this.camera,
+      document.querySelector("canvas.webgl")
+    );
+  }
+
+  resize() {
+    this.camera.aspect = this.sizes.width / this.sizes.height;
+    this.camera.updateProjectionMatrix();
+  }
+}
---
+++ src/Experience/Experience.js
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time.js";
+import Camera from "./Camera.js";
+
+let instance = null;
+
+export default class Experience {
+  constructor(canvas) {
+    if (instance) {
+      return instance;
+    }
+
+    instance = this;
+
+    // global access
+    window.experience = this;
+
+    // setup
+    this.sizes = new Sizes();
+    this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
+
+    // options
+    this.canvas = canvas;
+
+    console.log(this.canvas);
+
+    // resize event
+    this.sizes.on("resize", () => {
+      this.resize();
+    });
+
+    // time event
+    this.time.on("tick", () => {
+      this.update();
+    });
+  }
+
+  resize() {
+    this.camera.resize();
+  }
+
+  update() {}
+}
